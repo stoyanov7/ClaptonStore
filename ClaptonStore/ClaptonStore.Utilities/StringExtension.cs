cacbfa9e0b74e6f188f6cafc3b66c2cb711395d3
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-11-14 12:53:07 +0200

    Fix issue #1

---
+++ ClaptonStore/ClaptonStore.Utilities/StringExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace ClaptonStore.Utilities
+{
+    public static class StringExtension
+    {
+        public static string GetShortDescription(this string desc)
+        {
+            return $"{desc.Substring(0, 100)} . . .";
+        }
+    }
+}
--- ClaptonStore/ClaptonStore/Infrastructure/MappingProfile.cs
+++ ClaptonStore/ClaptonStore/Infrastructure/MappingProfile.cs
@@ -4,12 +4,15 @@
     using Models;
     using Models.ViewModels;
     using Services;
+    using Utilities;
 
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
-            this.CreateMap<Game, AllGamesViewModel>();
+            this.CreateMap<Game, AllGamesViewModel>()
+                .ForMember(x => x.Description,
+                    cfg => cfg.MapFrom(desc => desc.Description.GetShortDescription()));
 
             this.CreateMap<Game, GameDetailsViewModel>()
                 .ForMember(d => d.Developer, cfg => cfg.MapFrom(dev => dev.Developer.Title))
