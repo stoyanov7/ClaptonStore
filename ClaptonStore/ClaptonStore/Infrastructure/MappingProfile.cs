3790f40412d57de646da717b715b9510e76576f3
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-11-14 11:29:24 +0200

    Configure Automapper

---
+++ ClaptonStore/ClaptonStore/Infrastructure/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿namespace ClaptonStore.Infrastructure
+{
+    using AutoMapper;
+    using Models;
+    using Models.ViewModels;
+    using Services;
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            this.CreateMap<Game, AllGamesViewModel>();
+
+            this.CreateMap<Game, GameDetailsViewModel>()
+                .ForMember(d => d.Developer, cfg => cfg.MapFrom(dev => dev.Developer.Title))
+                .ForMember(x => x.Genre, cfg => cfg.MapFrom(genre => genre.GameGenreType.GetDisplayName()));
+        }
+    }
+}
--- ClaptonStore/ClaptonStore/Startup.cs
+++ ClaptonStore/ClaptonStore/Startup.cs
@@ -1,6 +1,7 @@
 ﻿namespace ClaptonStore
 {
     using System;
+    using AutoMapper;
     using Data;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
@@ -63,6 +64,8 @@
 
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddAutoMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
